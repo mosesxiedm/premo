f6af029775e1eed665d6db15d6c859ea84a84306
Moses Xie <moses.xie@dm-companies.com>
2017-09-26 17:53:11 -0400

    started working on index controller spec

--- spec/indexControllerSpec.php
+++ spec/indexControllerSpec.php
@@ -2,10 +2,16 @@
 namespace DM\MovieApp\Controllers;
 
 describe(MovieApi::class, function () {
-	describe("->indexAction", function () {
-		it("calls fetch movies", function (){
-			$indexController_instance = new indexController();
-			expect(count($indexController_instance->indexAction()))->toBeGreaterThan(0);
-		});
-	});
+    describe("->indexAction", function () {
+        it("calls fetch movies", function (){
+            $indexController_instance = new indexController();
+            expect(count($indexController_instance->indexAction()))->toBeGreaterThan(0);
+        });
+    });
+    describe("->storeMovies", function () {
+        it("stores movies into the database", function (){
+            $indexController_instance = new indexController();
+            expect(count($indexController_instance->indexAction()))->toBeGreaterThan(0);
+        });
+    });
 });
