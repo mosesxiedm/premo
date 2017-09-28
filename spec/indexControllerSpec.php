f6ffddb8cf7c2211f1f880753b3fbdb1c3df282d
Moses Xie <moses.xie@dm-companies.com>
2017-09-28 15:16:20 -0400

    d

--- spec/indexControllerSpec.php
+++ spec/indexControllerSpec.php
@@ -1,6 +1,11 @@
 <?php
 namespace DM\MovieApp\Controllers;
 
+use Kahlan\Plugin\Stub;
+use DM\MovieApp\Model\Movie;
+use DM\MovieApp\Services\MovieAPI;
+use \Phalcon\Mvc\Controller;
+
 describe(MovieApi::class, function () {
     describe("->indexAction", function () {
         it("calls fetch movies", function (){
@@ -15,12 +20,31 @@ describe(MovieApi::class, function () {
             expect(count($indexController_instance->getMoviesFromApi()))->toBeGreaterThan(2);
         });
     });
-    describe("->storeMovies", function(){
-        it("calls findFirst and returns empty if it already exists", function(){
-            $indexController_instance = new indexController();
-            $list_of_movies = $indexController_instance->getMoviesFromApi();
-            expect(count($list_of_movies))->toBeGreaterThan(2);
-        });
+    given('controller',function(){
+         $controller = Stub::create(['extends' => indexController::class]);
+         return $controller;
+    });
+    given('list_of_movies', function () {
+        $list_of_movies = array();
+        $movie = new Movie;
+        $movie->id = 12345;
+        $list_of_movies[] = $movie;
+        return $list_of_movies;
+            });
 
+    describe("->storeMovies", function(){
+        it("findFirst returns empty if it doesnt exist", function(){
+        });
+        it("saves if findFirst returns empty", function(){
+            expect(Movie::class)
+                ->toReceive('save');
+            Stub::on($this->controller)
+                ->method('::getMoviesFromApi')
+                ->andReturn(array());
+            Stub::on(Movie::class)
+                ->method('::findFirst')
+                ->andReturn();
+            $this->controller->storeMovies($this->list_of_movies);
+        });
     });
 });
