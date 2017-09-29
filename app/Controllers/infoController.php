eb98d5eafdd07732a6a86e32a3ce820c17478659
Moses Xie <moses.xie@dm-companies.com>
2017-09-29 17:30:36 -0400

    removed some comments

--- app/Controllers/infoController.php
+++ app/Controllers/infoController.php
@@ -9,13 +9,6 @@ class InfoController extends Controller
     public function infoAction($id)
     {
         $movie = Movie::findFirst($id);
-        /*
-        header('Content-type: image/jpeg;');
-
-        $url = "http://image.tmdb.org/t/p/w500" . $movie->poster_path;
-
-        $image = file_get_contents($url);
-        */
 
         $this->view->image =  $movie->poster_path;
 
